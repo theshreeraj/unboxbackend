4ddd9c26049e82f7000cd8c55bade8ee89d20469
theshreeraj <shreerajmane007@gmail.com>
2025-01-18 17:14:20 +0530

    [fix]:db fix

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("DB Connected");
-  });
-
-  await mongoose.connect(process.env.MONGODB_URI);
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Database connected successfully");
+  } catch (err) {
+    console.error("Error connecting to the database:", err.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
